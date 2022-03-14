e88343abc001bbbae54792fe8f6aae2ea2e94770
AhmadTaha24 <neroata@hotmail.com>
2022-03-14 12:31:59 +0200

    removing face from detected image to reduce noises using haar cascade

--- aa.py
+++ aa.py
@@ -1,4 +1,5 @@
 import cv2
+from cv2 import imshow
 import numpy as np
 import time
 
@@ -25,6 +26,7 @@ def detection():
     while True:
         
         ret, frame = capture.read()
+        frame = face_censoring(frame) #censoring face to reduce noise
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
         #lower and upper range
@@ -52,5 +54,28 @@ def detection():
         cv2.imshow("keyboard",framekey)
         if cv2.waitKey(1)==ord('q'):
             break
+def face_detection():
+    cap = cv2.VideoCapture(0)
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    while True:
+        ret, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), -1)            
+        cv2.imshow("face", frame)
+        cv2.waitKey(1)
+def face_censoring(frame):
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    
+        
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 0), -1)            
+    cv2.imshow("face", frame)
+    censored_frame = frame
+
+    return censored_frame
 print("hello world")
 detection()
